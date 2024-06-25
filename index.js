df84ae1fb3573cb84d03838e1e466ba28245716e
MichLadd <95909039+MichLadd@users.noreply.github.com>
2024-06-26 00:00:28 +0200

    feat(logistic): socketio temperature alert example implementation

--- index.js
+++ index.js
@@ -3,15 +3,42 @@ const cors = require('cors');
 const {connectDB} = require("./src/config/dbConnection");
 const dotenv = require('dotenv');
 const router = require('./src/routes/route')
-/*
-* Allow access from any subroute of http://localhost:3000
-* */
+const http = require('http');
+const socketClient = require('socket.io-client');
+
 const logisticControlServicePort = process.env.PORT || 4005;
 let corsOptions = {
     origin: new RegExp(`http:\/\/wms-logistic:${logisticControlServicePort}\/.*`),
 };
 dotenv.config();
 const app = express();
+
+const server = http.createServer(app);
+
+const gatewayUrl = process.env.GATEWAY_URL;
+const socket = socketClient(gatewayUrl);
+
+socket.on('connect', () => {
+    console.log(`Connected to gateway at ${gatewayUrl}`);
+});
+
+
+
+// Simula il monitoraggio della temperatura e invia alert quando necessario
+function monitorTemperature() {
+    const zone = 'Zona 1';
+    const temperature = Math.floor(Math.random() * 30); // Simula una lettura della temperatura
+
+    if (temperature < 18) { // Soglia di temperatura
+        console.log(`Temperature alert for ${zone}: ${temperature}°C`);
+        // Invia l'alert al gateway
+        socket.emit('temperature-alert', { zone, temperature });
+    }
+}
+
+setInterval(monitorTemperature, 5000); // Controlla la temperatura ogni 5 secondi
+
+
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger-doc.json'); // Importa il file di documentazione Swagger JSON
@@ -21,7 +48,11 @@ app.disable("x-powered-by");
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use(router);
-app.listen(logisticControlServicePort, () => console.info(`WMS-logisticControl-service is running`));
+
+server.listen(logisticControlServicePort, () => {
+    console.info(`WMS-logisticControl-service is running`);
+    console.log(`Temperature monitoring service running on port ${logisticControlServicePort}`);
+});
 
 connectDB(process.env.DB_NAME);
 
